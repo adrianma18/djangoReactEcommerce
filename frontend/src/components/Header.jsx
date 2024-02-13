fbd61c9a2ca779cd6f7445961d62d955051e3f67
adrianma18 <adrianmarten@gmail.com>
2024-02-13 21:15:44 +0100

    base

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Navbar, Nav, Container, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Neon Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarColor02" />
+          <Navbar.Collapse id="navbarColor02">
+            <Nav className="mr-auto">
+              <Nav.Link href="/cart" active><i className="fas fa-shopping-cart"></i> Cart <span className="visually-hidden">(current)</span></Nav.Link>
+              <Nav.Link href="/login" active><i className="fas fa-user"></i> Login <span className="visually-hidden">(current)</span></Nav.Link>
+            </Nav>
+
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
